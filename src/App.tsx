c7568024ac98e88443c453ce297593052826a31c
Alejandro Navarro <alexnavarro57@gmail.com>
2025-12-31 03:01:59 -0800

    Created initial game nav

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+      <>
+          <div className={"gameCard"}>
+              <h1>Correlations</h1>
+              <section id={"difficulty"}>
+                  <button className={"difficultyButton"}>Easy</button>
+                  <button className={"difficultyButton"}>Medium</button>
+                  <button className={"difficultyButton"}>Hard</button>
+              </section>
+          </div>
+      </>
+  )
+}
+
+export default App
