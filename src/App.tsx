fc91fac97b3067e87502a3a1f0fe956d0cde643d
Alejandro Navarro <alexnavarro57@gmail.com>
2025-12-31 15:54:26 -0800

    Created UI for player selection, difficulty selection, and word inputs.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,103 @@
+import {useState} from "react";
+
 function App() {
-  return (
+    const [state, setState] = useState("player");
+
+    return (
       <>
           <div className={"gameCard"}>
               <h1>Correlations</h1>
               <section id={"difficulty"}>
-                  <button className={"difficultyButton"}>Easy</button>
-                  <button className={"difficultyButton"}>Medium</button>
-                  <button className={"difficultyButton"}>Hard</button>
+                  {state === "player" && <Player goToDifficulty={() => setState("difficulty")} />}
+                  {state === "difficulty" && <Difficulty
+                      goToEasyInput={() => setState("easyInput")}
+                      goToMediumInput={() => setState("mediumInput")}
+                      goToHardInput={() => setState("hardInput")}/>}
+
+                  {state === "easyInput" && <EasyInput />}
+                  {state === "mediumInput" && <MediumInput />}
+                  {state === "hardInput" && <HardInput />}
               </section>
           </div>
       </>
-  )
+  );
 }
 
-export default App
+type PlayerProps = {
+    goToDifficulty: () => void;
+}
+
+function Player({ goToDifficulty }: PlayerProps) {
+    return (
+        <>
+            <button className={"difficultyButton"} onClick={goToDifficulty}>1 Player</button>
+            <button className={"difficultyButton"}>2 Player</button>
+        </>
+    );
+}
+
+type DifficultyProps = {
+    goToEasyInput: () => void;
+    goToMediumInput: () => void;
+    goToHardInput: () => void;
+}
+
+function Difficulty({ goToEasyInput, goToMediumInput, goToHardInput }: DifficultyProps) {
+    return (
+        <>
+            <button className={"difficultyButton"} onClick={goToEasyInput}>Easy</button>
+            <button className={"difficultyButton"} onClick={goToMediumInput}>Medium</button>
+            <button className={"difficultyButton"} onClick={goToHardInput}>Hard</button>
+        </>
+    );
+}
+
+function EasyInput() {
+    return (
+        <>
+            <p>Hint Word</p>
+            <input type="text"/>
+
+            <p>Guess Words</p>
+            {Array.from({length: 3}).map((_, index) => (
+                <input key={index} type="text"/>
+            ))}
+
+            <button>Submit</button>
+        </>
+    );
+}
+
+function MediumInput() {
+    return (
+        <>
+            <p>Hint Word</p>
+            <input type="text"/>
+
+            <p>Guess Words</p>
+            {Array.from({length: 5}).map((_, index) => (
+                <input key={index} type="text"/>
+            ))}
+
+            <button>Submit</button>
+        </>
+    );
+}
+
+function HardInput() {
+    return (
+        <>
+            <p>Hint Word</p>
+            <input type="text"/>
+
+            <p>Guess Words</p>
+            {Array.from({length: 7}).map((_, index) => (
+                <input key={index} type="text"/>
+            ))}
+
+            <button>Submit</button>
+        </>
+    );
+}
+
+export default App;
